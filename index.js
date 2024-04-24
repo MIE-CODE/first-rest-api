ef98baac5cc5ee91776665cff467f141bfe367bc
Meyangz <Kayfolayan614@gmail.com>
2024-04-24 20:34:55 +0100

    fixed the bug and now connected to mongodb

--- index.js
+++ index.js
@@ -5,7 +5,16 @@ const mongoose = require("mongoose");
 const app = express();
 const dbURI =
   "mongodb+srv://MIE:meyangs126@cluster0.slzl1aq.mongodb.net/blog-store?retryWrites=true&w=majority&appName=Cluster0";
-mongoose.connect(dbURI);
+const port = 5000;
+mongoose
+  .connect(dbURI)
+  .then((result) => {
+    app.listen(process.env.port || port, () => {
+      console.log(`now listening for request on port ${port}`);
+    });
+    console.log("connected to db");
+  })
+  .catch((err) => console.log(err));
 mongoose.Promise = global.Promise;
 app.use(bodyParser.json());
 app.use("/api", routes);
@@ -13,9 +22,7 @@ app.use((err, req, res, next) => {
   res.status(422).send({ error: err._message });
   console.log(err);
 });
-app.listen(process.env.port || 4000, () => {
-  console.log("now listening for request...");
-});
+
 app.get("/api", (req, res) => {
   res.send({
     name: "dave",
